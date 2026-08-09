880aca8376a31cde46c6895033e8adba98568e9c
Felipe Garcia <fe.garcia20241@gmail.com>
2026-08-09 11:05:04 -0300

    Subindo danki

--- RelatoriosMédicos/src/RelatoriosMedicos.java
+++ RelatoriosMédicos/src/RelatoriosMedicos.java
@@ -1,4 +1,12 @@
 public class RelatoriosMedicos {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String[] medicos = new String[5];
+        int[][] pacientes = new int[5][3];
+        medicos = {"Dr Fernando", "Dr Maria", "Dr Sonia", "Dr Pedro", "Dr Joao"};
 
+
+        for (int i=0; i < 5; i++) {
+        }
+    }
 }
---
+++ danki.py
@@ -0,0 +1,18 @@
+"""Danki"""
+
+a = "Olá Mundo"
+b = 5
+c = 3
+d = 1+2j
+
+print(a)
+print(type(b))
+print(type(c))
+print(type(d))
+
+x = 2
+
+if x ==3:
+    print("X é igual a 3")
+else:
+    print("X não é igual a 3")
